78df7c1e353c690bdb8782869fbf8e38150ee256
Andrés Felipe Herrera <128004097+anfherrera@users.noreply.github.com>
2025-09-01 14:17:24 -0500

    Primer commit del proyecto Front-end-gestion-curricular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'roles', component: HomeComponent }, // temporal
+      { path: 'empresas', component: HomeComponent } // temporal
+    ]
+  }
+];
