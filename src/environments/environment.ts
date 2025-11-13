f42df18c5a33d88e384cb36ebb39ecfe8ef7d0b0
Andrés Felipe Herrera <128004097+anfherrera@users.noreply.github.com>
2025-11-13 18:02:37 -0500

    chore: actualice la url de backkend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api' // Backend Java en puerto 5000
+  apiUrl: 'https://back-end-gestion-curricular.onrender.com/api'
 };
